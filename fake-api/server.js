8e5ced93bd3cda0a6f97226318c607646f7ba16f
m4rk055 <marko@blindnet.io>
2022-11-29 00:26:44 +0100

    Remove json-server

--- fake-api/server.js
+++ fake-api/server.js
@@ -1,27 +1,95 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('fake-api/db.json')
-const middlewares = jsonServer.defaults()
+const express = require("express")
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-server.use(middlewares)
-server.use(jsonServer.bodyParser)
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1'
-}))
+var db = {
+  "account": {
+    "email": "test@blindnet.io"
+  },
+  "app-groups": [
+    {
+      "id": "group_1",
+      "name": "group_one",
+      "description": "this is group one",
+      "key": "key_1",
+      "applications": [
+        {
+          "id": "group_1_app_1",
+          "name": "group_one_app_1",
+          "active": true
+        },
+        {
+          "id": "group_1_app_2",
+          "name": "group_one_app_2",
+          "active": true
+        },
+        {
+          "id": "group_1_app_3",
+          "name": "group_one_app_3",
+          "active": false
+        }
+      ]
+    },
+    {
+      "id": "group_2",
+      "name": "group_two",
+      "description": "this is group two",
+      "key": "key_2",
+      "applications": [
+        {
+          "id": "group_2_app_1",
+          "name": "group_two_app_1",
+          "active": true
+        },
+        {
+          "id": "group_2_app_2",
+          "name": "group_two_app_2",
+          "active": true
+        }
+      ]
+    },
+    {
+      "id": "group_3",
+      "name": "group_three",
+      "key": "key_3",
+      "description": null,
+      "applications": []
+    }
+  ]
+}
 
-server.post('/login', (req, res) => {
+const server = express()
+server.use(bodyParser.json())
+server.use(cors())
+
+server.post('/api/login', (req, res) => {
   res.jsonp({ token: "token_123" })
 })
 
-server.put('/app-groups', (req, res, next) => {
+server.get('/api/app-groups', (req, res) => {
+  res.send(db['app-groups'])
+})
+
+server.get('/api/account', (req, res) => {
+  res.send(db['account'])
+})
+
+server.put('/api/app-groups', (req, res, next) => {
   const id = (Math.random() + 1).toString(36).substring(7)
   req.body.id = id
   req.body.applications = []
-  req.method = 'POST'
-  next()
+  db['app-groups'].push(req.body)
+  res.send({ id })
 })
 
-server.use(router)
+server.put('/api/app-groups/:id/applications', (req, res, next) => {
+  const id = (Math.random() + 1).toString(36).substring(7)
+  req.body.id = id
+  req.body.active = true
+  db['app-groups'] = db['app-groups'].map(g => g.id == req.params.id ? { ...g, applications: [...g.applications, req.body] } : g)
+  res.send({ id })
+})
+
 server.listen(5000, () => {
   console.log('JSON Server is running')
 })
