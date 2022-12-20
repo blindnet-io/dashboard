39fadea3c0b6c44f7a8b1016fd5caececcc8bfff
m4rk055 <marko@blindnet.io>
2022-12-20 15:04:58 +0100

    Fix section header close button

--- src/components/apps/SectionHeader.tsx
+++ src/components/apps/SectionHeader.tsx
@@ -2,17 +2,16 @@ import { Link } from 'react-router-dom';
 
 export function SectionHeader({ name }: { name?: string }) {
   return (
-    <div className="row mb-5">
-      <div className="col-md-6 col-12">{name && <h2>{name}</h2>}</div>
-      <div className="col-6 d-none d-md-block text-end">
+    <div className="d-md-flex">
+      <div className="me-auto">{name && <h2>{name}</h2>}</div>
+      <div className="d-none d-md-block pt-1">
         <Link to="/">
           <button
             type="button"
             className="btn-close"
             aria-label="Close"
           ></button>
-        </Link>
-      </div>
+        </Link></div>
     </div>
   );
 }
