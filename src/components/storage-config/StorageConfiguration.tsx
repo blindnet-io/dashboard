e72cedde27ab2d09352dd071a779ecdccb10d923
m4rk055 <marko@blindnet.io>
2023-01-13 17:41:40 +0100

    Remove dca token config form

--- src/components/storage-config/StorageConfiguration.tsx
+++ src/components/storage-config/StorageConfiguration.tsx
@@ -1,17 +1,10 @@
 import Accordion from 'react-bootstrap/Accordion';
 import { ApplicationInfo } from '../../store/appsSlice';
 import ConnectorsConfig from './connectors/ConnectorsConfig';
-import TokenConfig from './token/TokenConfig';
 
 export function StorageConfiguration({ app }: { app: ApplicationInfo }) {
   return (
     <Accordion className="overflow-auto" defaultActiveKey="connectors">
-      <Accordion.Item eventKey="token">
-        <Accordion.Header>API token</Accordion.Header>
-        <Accordion.Body>
-          <TokenConfig token={app.dacToken} />
-        </Accordion.Body>
-      </Accordion.Item>
       <Accordion.Item eventKey="connectors">
         <Accordion.Header>Connectors</Accordion.Header>
         <Accordion.Body>
